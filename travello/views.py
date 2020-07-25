6bdd2a3de86344ca3b9d9c35df93a2372b024354
lijinho <58988274+lijinho@users.noreply.github.com>
2020-07-25 11:52:13 +0200

    dynamic fix

--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 
 def index(request):
-  return render(request, "index.html")
+
+  dest1 = Destination()
+  dest1.name = "Mumbui"
+  dest1.desc = "The city That never Sleeps"
+  dest1.image = "destination_1.jpg"
+  dest1.price = 700
+
+  dest2 = Destination()
+  dest2.name = "Hyderabad"
+  dest2.desc = "First Biryani, Then Sherwani"
+  dest2.image = "destination_2.jpg"
+  dest2.price = 650
+
+  dest3 = Destination()
+  dest3.name = "Bengaluru"
+  dest3.desc = "Beautiful City"
+  dest3.image = "destination_3.jpg"
+  dest3.price = 750
+  
+  dests = [dest1, dest2, dest3]
+
+  return render(request, "index.html", {'dests': dests})
