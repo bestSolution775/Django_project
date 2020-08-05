cc41f2e2eeb08c9a7fe15e347fe3a781072ebddc
lijinho <58988274+lijinho@users.noreply.github.com>
2020-08-05 11:10:33 +0200

    complete database model

--- travello/views.py
+++ travello/views.py
@@ -9,19 +9,22 @@ def index(request):
   dest1.desc = "The city That never Sleeps"
   dest1.image = "destination_1.jpg"
   dest1.price = 700
+  dest1.offer = False
 
   dest2 = Destination()
   dest2.name = "Hyderabad"
   dest2.desc = "First Biryani, Then Sherwani"
   dest2.image = "destination_2.jpg"
   dest2.price = 650
+  dest2.offer = True
 
   dest3 = Destination()
   dest3.name = "Bengaluru"
   dest3.desc = "Beautiful City"
   dest3.image = "destination_3.jpg"
   dest3.price = 750
-  
+  dest3.offer = True
+
   dests = [dest1, dest2, dest3]
 
   return render(request, "index.html", {'dests': dests})
